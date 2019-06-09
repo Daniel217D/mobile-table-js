d766a5aea9475073a72ca29eab033b4057c1498e
Daniel217D <daniel217032001@gmail.com>
2019-06-09 14:31:58 +0300

    update shadows

--- src/tablichka.js
+++ src/tablichka.js
@@ -1,18 +1,47 @@
 import './tablichka.scss'
 
 class Tablichka {
-    constructor (options) {
+    constructor(options) {
         if (options instanceof Element || options instanceof HTMLDocument) {
-            options = {table: options}
+            this.options = {table: options}
+        } else if (typeof options === 'object' && options !== null) {
+            this.options = options;
+        } else {
+            throw new Error('Something wrong with your options');
         }
 
-        const wrapper = document.createElement('div');
-        wrapper.classList.add('tablichka');
-        options.table.parentNode.insertBefore(wrapper,options.table);
-        wrapper.appendChild(options.table);
+        this.wrapper = document.createElement('div');
+        this.wrapper.classList.add('tablichka');
+        this.options.table.parentNode.insertBefore(this.wrapper, options.table);
+        this.wrapper.appendChild(this.options.table);
+
+        this.shadow_l = document.createElement('div');
+        this.shadow_r = document.createElement('div');
+
+        this.shadow_l.classList.add('tablichka-shadow', 'tablichka-shadow_left');
+        this.shadow_r.classList.add('tablichka-shadow', 'tablichka-shadow_right');
+        this.wrapper.appendChild(this.shadow_l);
+        this.wrapper.appendChild(this.shadow_r);
+
+        window.addEventListener('resize', this.resizeHandler.bind(this))
+        this.wrapper.addEventListener('scroll', this.scrollHandler.bind(this))
+    }
+
+    resizeHandler = () => {
+        if (this.wrapper.offsetWidth >= this.options.table.offsetWidth) {
+            this.wrapper.classList.add('tablichka_noshadow')
+        } else {
+            this.wrapper.classList.remove('tablichka_noshadow');
+            this.scrollHandler();
+        }
+    };
+
+    scrollHandler = () => {
+        this.shadow_l.style.left = this.wrapper.scrollLeft + 'px';
+        this.shadow_r.style.right = -this.wrapper.scrollLeft +'px'
     }
 }
 
-export default function tablichka (options) {
+export default function tablichka(options) {
     return new Tablichka(options)
 }
