d0556c542908d8cfb5b1fc0a2d3bbae396a24e7c
Daniel217D <daniel217032001@gmail.com>
2019-06-25 00:21:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 import tablichka from './src/tablichka.js'
 
-if (process.env.NODE_ENV === 'development') {
-    import('./src/example.scss').then();
-}
-
 tablichka(document.getElementById('tablichka'));
